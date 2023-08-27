0c368d068054a66f4804298cbf65ecea40b60f8a
Tomás Bohn Guixeras <bohnguixeras@gmail.com>
2023-08-27 20:52:19 +0200

    Hero basic layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import IconButton from "@/components/IconButton";
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
@@ -6,8 +7,8 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <main id="container" className=" w-5/6">
-        <div id="hero-section">
+      <main id="container" className="w-5/6">
+        <div id="hero-section" className="flex flex-col items-center">
           <Image
             src="/tomas-hero.png"
             width={500}
@@ -15,12 +16,31 @@ export default function Home() {
             alt="Tomas Bohn Guixeras profile picture"
           />
 
-          <div id="text-container">
-            <h1>Tomas Bohn Guixeras</h1>
-            <h4>Fullstack Developer</h4>
+          <div id="text-container" className="text-center">
+            <h1 className="text-3xl font-bold bg-gradient-to-r from-[#FFC25C] to-[#ffe551] text-transparent bg-clip-text">
+              Tomas Bohn Guixeras
+            </h1>
+            <h4 className="text-2xl">Fullstack Developer</h4>
           </div>
 
-          <div id="icon-buttons-container"></div>
+          <div id="icon-buttons-container" className="flex w-fit my-10">
+            <IconButton
+              icon={"linkedin"}
+              url={"https://www.linkedin.com/in/tomasbohnguixeras/"}
+            />
+            <IconButton
+              icon={"github"}
+              url={"https://github.com/tomasguixeras"}
+            />
+            <IconButton icon={"x"} url={"https://twitter.com/tomasguixeras"} />
+          </div>
+
+          <div
+            id="technologies-carrusel"
+            className="text-center w-screen py-2 bg-[#FFC25C] text-blue-950"
+          >
+            <p>Recat - Typescript</p>
+          </div>
         </div>
       </main>
       <Footer />
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-function IconButton() {
+function IconButton({icon, url}: any) {
   return (
-    <div>IconButton</div>
-  )
+    <div className="flex items-center justify-center border-2 w-14 h-14 rounded-full m-2">
+      <a href={url}>
+        <Image
+          src={`/social-icons/${icon}-logo.svg`}
+          alt="Social Icon Button"
+          height={30}
+          width={30}
+        />
+      </a>
+    </div>
+  );
 }
 
-export default IconButton
+export default IconButton;
