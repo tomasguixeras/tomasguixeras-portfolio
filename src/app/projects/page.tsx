7fa2a719d56fb1ff84f002cc71c1eaa4463becb3
Tomás Bohn Guixeras <bohnguixeras@gmail.com>
2023-08-28 13:05:54 +0200

    Styles fixing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,9 +6,9 @@ import React from "react";
 function page() {
   return (
     <div className="w-5/6 h-screen flex flex-col items-center justify-center">
-      <div className="border w-5/6 h-3/6 flex flex-col items-center justify-center text-3xl">
-        <p>We are working on this view.</p>
-        <p>It would be available soon.</p>
+      <div className="w-5/6 h-3/6 flex flex-col items-center justify-center text-3xl">
+        <p className="my-2 text-center">We are working on this view.</p>
+        <p className="my-2 text-center">It would be available soon.</p>
       </div>
 
       <p className="mt-20 italic font-semibold">If you need more information</p>
