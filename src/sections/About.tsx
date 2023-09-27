05e3d7cff63182d4a206b4349417aa88cc45c473
Tomás Bohn Guixeras <bohnguixeras@gmail.com>
2023-09-27 11:37:00 +0200

    Changing language translate with next-intl

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,12 +5,15 @@ import AboutButtons from "@/components/AboutButtons";
 import { languageValidator } from "@/utils/languageValidator";
 import { AboutInterface, AboutTexts } from "@/types/json-data/about-types";
 import aboutData from "@/data/about-texts.json";
+import { useTranslations } from "next-intl";
 
 interface AboutProps {
   screenSize: number;
 }
 
 function About({ screenSize }: AboutProps) {
+  const t = useTranslations("About");
+  const keys = ["calendly", "curriculum"] as const;
   const [aboutText, setAboutText] = useState<AboutTexts>();
 
   useEffect(() => {
@@ -22,25 +25,22 @@ function About({ screenSize }: AboutProps) {
       <div className="w-full flex flex-row items-center">
         <div className="flex-1 flex flex-col h-fit lg:mr-6">
           <h3 className="text-3xl font-bold bg-gradient-to-r from-white to-[#FFC25C] text-transparent bg-clip-text w-fit mb-6 md:text-4xl">
-            {aboutText && aboutText.title}
+            {t("title")}
           </h3>
-          <p>{aboutText && aboutText.description}</p>
+          <p>{t("text")}</p>
           {screenSize >= 1024 && (
             <div className="flex flex-row mt-12">
-              {typeof aboutText !== "undefined" && (
-                <>
-                  <AboutButtons
-                    icon={aboutText.buttons.calendly.icon}
-                    title={aboutText.buttons.calendly.title}
-                    url={aboutText.buttons.calendly.url}
-                  />
-                  <AboutButtons
-                    icon={aboutText.buttons.curriculum.icon}
-                    title={aboutText.buttons.curriculum.title}
-                    url={aboutText.buttons.curriculum.url}
-                  />
-                </>
-              )}
+              {typeof aboutText !== "undefined" &&
+                keys.map((key: string) => {
+                  return (
+                    <AboutButtons
+                      key={key}
+                      icon={t(`actionButtons.${key}.icon`)}
+                      title={t(`actionButtons.${key}.title`)}
+                      url={t(`actionButtons.${key}.url`)}
+                    />
+                  );
+                })}
             </div>
           )}
         </div>
