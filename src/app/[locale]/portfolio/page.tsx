cbcd6db18adc83b9686c16bf8fee55b30a040e4d
Tomás Bohn Guixeras <bohnguixeras@gmail.com>
2023-11-28 18:52:15 +0100

    Adding basic full projects page

--- src/app/[locale]/portfolio/page.tsx
+++ src/app/[locale]/portfolio/page.tsx
@@ -1,10 +1,36 @@
-"use client"
-import BuildingPage from "@/components/BuildingPage";
+"use client";
 import React from "react";
+import { useTranslations } from "next-intl";
+
+import ProjectCard from "@/components/ProjectCard";
 
 function Projects() {
+  const t = useTranslations("Portfolio");
+  const projects = [
+    "HenryDogApp",
+    "MarketsCenter",
+    "42iCripto",
+    "ColorTester",
+    "reverseWords",
+    "tomasGuixeras",
+    "gamePlugins",
+  ];
   return (
-    <BuildingPage />
+    <div className="w-11/12 grid grid-cols-1 gap-4 my-10 md:gap-6 md:grid-cols-2 lg:grid-cols-3 lg:mt-10 lg:mb-12">
+      {projects &&
+        projects.map((project: string) => {
+          return (
+            <ProjectCard
+              key={project}
+              id={t(`projects.${project}.id`)}
+              name={t(`projects.${project}.name`)}
+              description={t(`projects.${project}.description`)}
+              image={t(`projects.${project}.image`)}
+              link={t(`projects.${project}.link`)}
+            />
+          );
+        })}
+    </div>
   );
 }
 
